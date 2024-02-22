1485ff948e628658044e292feb651d4c74263de6
serg <feelwork2077@gmail.com>
2024-02-22 13:23:46 +0100

    home page

--- meteo_app/urls.py
+++ meteo_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from meteo_app import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('settings/', views.SettingsView.as_view(), name='settings'),
+    path('sea/', views.SeaView.as_view(), name='sea'),
+    path('about/', views.AboutView.as_view(), name='about'),
 ]
--- meteo_app/views.py
+++ meteo_app/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+
+class SettingsView(TemplateView):
+    template_name = 'settings.html'
+
+
+class SeaView(TemplateView):
+    template_name = 'sea.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
