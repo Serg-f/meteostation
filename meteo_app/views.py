d4572ee7ea6449c001b83246a190321b9029b878
serg <feelwork2077@gmail.com>
2024-02-22 16:27:45 +0100

    base front ready

--- meteo_app/views.py
+++ meteo_app/views.py
@@ -3,15 +3,19 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+    extra_context = {'menu': 0}
 
 
 class SettingsView(TemplateView):
     template_name = 'settings.html'
+    extra_context = {'menu': 1}
 
 
 class SeaView(TemplateView):
     template_name = 'sea.html'
+    extra_context = {'menu': 2}
 
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+    extra_context = {'menu': 3}
