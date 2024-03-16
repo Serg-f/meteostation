be54aee958b2c04a9ee661a27be63655a0c40886
serg <feelwork2077@gmail.com>
2024-03-16 21:57:42 +0100

    SSE async issue resolved

--- main/src/meteo_app/views.py
+++ main/src/meteo_app/views.py
@@ -1,29 +1,29 @@
-import redis
+import json
+import logging
+from datetime import datetime
+from asgiref.sync import sync_to_async
 from django.http import StreamingHttpResponse
 from django.views import View
 from rest_framework.response import Response
+import aioredis
+
 from .models import ControllerData
 from .serializers import ControllerDataSerializer
-from asgiref.sync import sync_to_async
-import json
-from datetime import datetime
-import logging
 
 # Configure logger
 logger = logging.getLogger(__name__)
 
 
-# Async event stream function
 async def event_stream():
     logger.debug("Starting event stream")
-    r = redis.Redis(host='redis', port=6379, db=0)
-    pubsub = r.pubsub()
-    pubsub.subscribe('weather_data_channel')
+    redis = aioredis.from_url("redis://redis:6379/0", encoding="utf-8", decode_responses=True)
+    pubsub = redis.pubsub()
 
-    for message in pubsub.listen():
+    await pubsub.subscribe('weather_data_channel')
+
+    async for message in pubsub.listen():
         if message['type'] == 'message':
             try:
-                # Use sync_to_async to fetch the last data asynchronously
                 new_data = await sync_to_async(ControllerData.objects.last)()
                 if new_data:
                     send_data = ControllerDataSerializer(new_data).data
@@ -35,10 +35,8 @@ async def event_stream():
             except Exception as e:
                 logger.error(f"Error during event stream: {str(e)}", exc_info=True)
                 break  # Exit the loop if an error occurs
-    logger.debug("Ending event stream")
 
 
-# Async SSE view
 class WeatherDataSSEView(View):
     async def get(self, request):
         logger.debug("Request received for WeatherDataSSEView")
