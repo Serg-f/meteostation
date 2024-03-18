e6b5a519f399df153ea31cf61454205b264eee25
serg <feelwork2077@gmail.com>
2024-03-18 12:05:49 +0100

    Prevent chart destroy, set debounce time

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, {useState, useEffect, useRef, useCallback} from "react";
 import BaseLayout from "./BaseLayout";
 import {Chart, registerables} from 'chart.js';
 import 'chartjs-adapter-date-fns';
@@ -8,11 +8,9 @@ import '../css/dashboard.css';
 Chart.register(...registerables);
 
 const Dashboard = () => {
-        // Calculate yesterday's date
         const yesterday = new Date();
         yesterday.setDate(yesterday.getDate() - 1);
 
-        // Calculate today's date
         const today = new Date();
 
         // Format dates to YYYY-MM-DDTHH:MM as required for datetime-local input value
@@ -24,13 +22,14 @@ const Dashboard = () => {
             const minutes = (`0${date.getMinutes()}`).slice(-2);
             return `${year}-${month}-${day}T${hours}:${minutes}`;
         };
-
         const [parameter, setParameter] = useState('temperature_average');
         const [startDatetime, setStartDatetime] = useState(formatDate(yesterday));
         const [endDatetime, setEndDatetime] = useState(formatDate(today));
         const chartRef = useRef(null);
         const ws = useRef(null);
         const chartInstanceRef = useRef(null);
+        const previousParameter = useRef(parameter);
+        const isParameterChanged = useRef(false);
 
         const parameters = [
             {value: 'atm_pressure', label: 'Atmospheric Pressure'},
@@ -41,45 +40,78 @@ const Dashboard = () => {
             {value: 'wind_speed', label: 'Wind Speed'},
         ];
 
+        const requestData = useCallback(() => {
+            if (ws.current.readyState === WebSocket.OPEN) {
+                ws.current.send(JSON.stringify({parameter, start: startDatetime, end: endDatetime}));
+            } else {
+                console.error('WebSocket is not open. Cannot send data.');
+            }
+        }, [startDatetime, endDatetime, parameter]); // Include all dependencies here
+
+        // Now you can use requestData in useEffect or other useCallback hooks
+        const debounceTimerRef = useRef();
+
+        // Debounce function for data fetching
+        // Debounce function for data fetching
+        const debounceFetchData = useCallback(() => {
+            if (debounceTimerRef.current) {
+                clearTimeout(debounceTimerRef.current);
+            }
+            debounceTimerRef.current = setTimeout(() => {
+                requestData();
+            }, 500); // Debounce timeout
+        }, [requestData]);
+
         const handleDrag = (type) => (event) => {
             event.preventDefault();
-            const originalX = event.clientX;
+            let originalX = event.clientX;
             const originalDate = type === 'start' ? new Date(startDatetime) : new Date(endDatetime);
+            let lastFetchX = event.clientX; // Track the last X position that triggered a fetch
 
-            const updateDate = (moveEvent) => {
-                const diffX = moveEvent.clientX - originalX;
-                const speed = Math.max(1, Math.abs(diffX) / 100);
-                let newDate = new Date(originalDate.getTime() + diffX * 60 * 1000 * speed);
+            const moveHandler = (moveEvent) => {
+                const dx = moveEvent.clientX - originalX;
+                const fetchDx = moveEvent.clientX - lastFetchX;
+                const timeAdjustment = Math.round(dx / 5) * 60 * 60 * 1000;
+
+                const newDate = new Date(originalDate.getTime() + timeAdjustment);
+                const newDatetime = formatDate(newDate);
 
                 if (type === 'start') {
-                    setStartDatetime(formatDate(newDate));
+                    setStartDatetime(newDatetime);
                 } else {
-                    setEndDatetime(formatDate(newDate));
+                    setEndDatetime(newDatetime);
+                }
+
+                if (Math.abs(fetchDx) > 20) { // Check if drag distance since last fetch exceeds 20px
+                    debounceFetchData(); // Trigger data fetching
+                    lastFetchX = moveEvent.clientX; // Reset last fetch position
                 }
             };
 
-            const stopDrag = () => {
-                document.removeEventListener('mousemove', updateDate);
-                document.removeEventListener('mouseup', stopDrag);
+            const upHandler = () => {
+                document.removeEventListener('mousemove', moveHandler);
+                document.removeEventListener('mouseup', upHandler);
+                if (Math.abs(event.clientX - lastFetchX) <= 10) {
+                    // If the user releases the mouse without further dragging,
+                    // or moves the pointer back to the initial position, force a data fetch
+                    requestData();
+                }
             };
 
-            document.addEventListener('mousemove', updateDate);
-            document.addEventListener('mouseup', stopDrag);
+            document.addEventListener('mousemove', moveHandler);
+            document.addEventListener('mouseup', upHandler);
         };
 
 
         useEffect(() => {
-            function initializeWebSocket() {
+            const initializeWebSocket = () => {
                 ws.current = new WebSocket('ws://localhost:8000/ws/dashboard/');
-
                 ws.current.onopen = () => {
                     console.log('WebSocket Connected');
                     requestData();
                 };
 
-                ws.current.onmessage = (event) => {
-                    handleWebSocketMessage(event);
-                };
+                ws.current.onmessage = handleWebSocketMessage;
 
                 ws.current.onerror = (error) => {
                     console.error('WebSocket Error:', error);
@@ -88,127 +120,112 @@ const Dashboard = () => {
                 ws.current.onclose = () => {
                     console.log('WebSocket Disconnected');
                 };
-            }
+            };
 
             initializeWebSocket();
 
             return () => ws.current?.close();
         }, []);
 
-        const requestData = () => {
-            if (ws.current.readyState === WebSocket.OPEN) {
-                ws.current.send(JSON.stringify({parameter, start: startDatetime, end: endDatetime}));
-            } else {
-                console.error('WebSocket is not open. Cannot send data.');
-            }
-        };
 
         const handleWebSocketMessage = (event) => {
             const {data} = JSON.parse(event.data);
-            updateChart(data);
-        };
-
-        const updateChart = (data) => {
-            if (chartInstanceRef.current) {
-                chartInstanceRef.current.destroy();
-            }
-
-            const calculateTimeDiffInDays = (startDateString, endDateString) => {
-                const startDate = new Date(startDateString);
-                const endDate = new Date(endDateString);
-                return (endDate - startDate) / (1000 * 60 * 60 * 24); // Difference in days
-            };
-
-            const timeDiffInDays = calculateTimeDiffInDays(startDatetime, endDatetime);
-            let timeUnit = 'day';
-
-            if (timeDiffInDays < 1) {
-                timeUnit = 'hour';
-            } else if (timeDiffInDays <= 7) {
-                timeUnit = 'day';
-            } else if (timeDiffInDays <= 30) {
-                timeUnit = 'week';
-            } else {
-                timeUnit = 'month';
-            }
-
-            const ctx = chartRef.current.getContext('2d');
-            chartInstanceRef.current = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: data.map(d => d.timestamp),
-                    datasets: [{
-                        label: parameters.find(p => p.value === parameter).label,
-                        data: data.map(d => d.value),
-                        backgroundColor: 'rgba(0, 0, 0, 0.7)', // Adjusted background color
-                        borderColor: 'rgb(75, 192, 192)',
-                        tension: 0.1
-                    }]
-                },
-                options: {
-                    scales: {
-                        x: {
-                            type: 'time',
-                            time: {
-                                tooltipFormat: 'dd MMM yyyy HH:mm',
-                                unit: timeUnit
-                            },
-                            title: {
-                                display: true,
-                                text: 'Date and Time'
-                            },
-                            ticks: {
-                                color: '#FFF' // Improved visibility for ticks
-                            },
-                            grid: {
-                                color: 'rgba(255,255,255,0.3)' // Lighter grid lines
-                            }
-                        },
-                        y: {
-                            title: {
-                                display: true,
-                                text: 'Value'
-                            },
-                            ticks: {
-                                color: '#FFF' // Improved visibility for ticks
-                            },
-                            grid: {
-                                color: 'rgba(255,255,255,0.3)' // Lighter grid lines
-                            }
-                        }
-                    },
-                    plugins: {
-                        legend: {
-                            labels: {
-                                color: 'white' // Ensure text is visible on the darker background
-                            }
-                        }
-                    },
-                    maintainAspectRatio: false,
-                    elements: {
-                        line: {
-                            borderWidth: 3
-                        },
-                        point: {
-                            radius: 4 // Make points more visible
-                        }
-                    },
-                    layout: {
-                        padding: {
-                            top: 10,
-                            left: 15,
-                            right: 15,
-                            bottom: 10
-                        }
-                    }
-                }
-            });
-
+            updateChart(data, isParameterChanged.current);
+            isParameterChanged.current = false; // Reset after handling
         };
 
         useEffect(() => {
+            isParameterChanged.current = previousParameter.current !== parameter;
             requestData();
+            previousParameter.current = parameter; // Update after sending request
         }, [parameter, startDatetime, endDatetime]);
+
+        const updateChart = (data, parameterChanged) => {
+            if (chartInstanceRef.current && !parameterChanged) {
+                const chart = chartInstanceRef.current;
+                chart.data.labels = data.map(d => d.timestamp);
+                chart.data.datasets.forEach((dataset) => {
+                    dataset.data = data.map(d => d.value);
+                    dataset.label = parameters.find(p => p.value === parameter).label;
+                });
+                chart.update();
+            } else {
+                if (chartInstanceRef.current) {
+                    chartInstanceRef.current.destroy();
+                }
+                const ctx = chartRef.current.getContext('2d');
+                chartInstanceRef.current = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: data.map(d => d.timestamp),
+                        datasets: [{
+                            label: parameters.find(p => p.value === parameter).label,
+                            data: data.map(d => d.value),
+                            backgroundColor: 'rgba(0, 0, 0, 0.7)', // Adjusted background color
+                            borderColor: 'rgb(75, 192, 192)',
+                            tension: 0.1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            x: {
+                                type: 'time',
+                                time: {
+                                    tooltipFormat: 'dd MMM yyyy HH:mm',
+                                    unit: 'hour' // Adjust this as necessary based on your data
+                                },
+                                title: {
+                                    display: true,
+                                    text: 'Date and Time'
+                                },
+                                ticks: {
+                                    color: '#FFF' // Improved visibility for ticks
+                                },
+                                grid: {
+                                    color: 'rgba(255,255,255,0.3)' // Lighter grid lines
+                                }
+                            },
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: 'Value'
+                                },
+                                ticks: {
+                                    color: '#FFF' // Improved visibility for ticks
+                                },
+                                grid: {
+                                    color: 'rgba(255,255,255,0.3)' // Lighter grid lines
+                                }
+                            }
+                        },
+                        plugins: {
+                            legend: {
+                                labels: {
+                                    color: 'white' // Ensure text is visible on the darker background
+                                }
+                            }
+                        },
+                        maintainAspectRatio: false,
+                        elements: {
+                            line: {
+                                borderWidth: 3
+                            },
+                            point: {
+                                radius: 4 // Make points more visible
+                            }
+                        },
+                        layout: {
+                            padding: {
+                                top: 10,
+                                left: 15,
+                                right: 15,
+                                bottom: 10
+                            }
+                        }
+                    }
+                });
+            }
+        };
 
         return (
             <BaseLayout>
